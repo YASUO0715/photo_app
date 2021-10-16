6e7a6ab99e4c7bfb493ddfc3c0e2a88643d86cb6
YASUO0715 <yasuo0715niihori@gmail.com>
2021-10-16 11:13:39 +0900

    詳細画面作成

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -5,10 +5,13 @@
     <section class="row" data-masonry='{ "percentPosition": true }'>
         @foreach ($articles as $article)
             <div class="col-6 col-md-4 col-lg-3 col-sl-2 mb-4">
-                <article class="card shadow-1">
+
+                <article class="card position-relative">
                     <img src="{{ $article->image_url }}" class="card-img-top">
                     <div class="card-title mx-3">
-                        {{ $article->caption }}
+                        <a href="{{ route('articles.show', $article) }}" class="text-decoration-none stretched-link">
+                            {{ $article->caption }}
+                        </a>
                     </div>
                 </article>
             </div>
