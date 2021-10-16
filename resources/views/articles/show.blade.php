3ff22a5c30cb9cbb134d2b6e1403b603e76f7cdc
YASUO0715 <yasuo0715niihori@gmail.com>
2021-10-16 13:34:46 +0900

    編集画面作成

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,29 +1,32 @@
 @extends('layouts.app')
 @section('title', '詳細画面')
 @section('content')
-<h1>画像詳細</h1>
-<section>
+    <h1>画像詳細</h1>
+    <section>
 
-    <article class="card shadow">
+        <article class="card shadow position-relative">
 
-        <figure class="m-3">
-            <div class="row">
-                <div class="col-6">
+            <figure class="m-3">
+                <div class="row">
+                    <div class="col-6">
 
-                    <img src="{{ $article->image_url }}" width="100%">
+                        <img src="{{ $article->image_url }}" width="100%">
+                    </div>
+                    <div class="col-6">
+                        <figcaption>
+                            <h1>
+                                {{ $article->caption }}
+                            </h1>
+                            <h3>
+                                {{ $article->info }}
+                            </h3>
+                        </figcaption>
+                    </div>
                 </div>
-                <div class="col-6">
-                    <figcaption>
-                        <h1>
-                            {{ $article->caption }}
-                        </h1>
-                        <h3>
-                            {{ $article->info }}
-                        </h3>
-                    </figcaption>
-                </div>
-            </div>
-        </figure>
-    </article>
-</section>
+            </figure>
+            <a href="{{ route('articles.edit', $article) }}">
+                <i class="fas fa-edit position-absolute top-0 end-0 fs-1"></i>
+            </a>
+        </article>
+    </section>
 @endsection
